cacb97f0cb2df0b993320be82b42f58100b58813
alfonso.domenici <alfonso.domenici@gmail.com>
2020-02-20 17:17:21 +0100

    corretti files Docker

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM dr.ad.arpa.piemonte.it:5000/wildfly-labins
 COPY ./target/labins.war ${DEPLOYMENT_DIR}
+COPY ./target/labins-fr.war ${DEPLOYMENT_DIR}
---
+++ buildAll.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#genero war del server
+mvn clean package
+
+#muovo in webapp
+cd ./../build-client/src/main/webapp
+echo -----------$(pwd)---------------
+rm -R *
+cp -R ./../../../WEB-INF/ .
+
+#copio i sorgenti del client
+cp -R ./../../../../client/src/* .
+cp -R ./../../../../client/node_modules ./
+
+#torno in build-client
+cd ./../../../
+echo -----------$(pwd)---------------
+
+#genero war del client
+mvn clean package
+cp ./target/labins-fr.war ./../server/target/
+
+#muovo nel server
+cd ./../server/
+echo -----------$(pwd)---------------
+
+docker build --rm -t dr.ad.arpa.piemonte.it:5000/labins .
+docker rm -f labins || true && docker run -d --network arpa -p 8080:8080 -p 4848:4848 --name labins dr.ad.arpa.piemonte.it:5000/labins  
